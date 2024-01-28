fe36fb6224299662db688a3b1fca523af29a2fb2
MD Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-01-28 17:50:53 +0600

    removed unnessery part in production

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -7,10 +6,6 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -22,20 +17,5 @@ app.use("/", indexRouter);
 
 app.use(express.static("public"));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
 
 module.exports = app;
